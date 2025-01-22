dd6a8ce6a914e3efe583a75b0c16a480d345a477
ethan-logue <ethanlogue03@gmail.com>
2025-01-22 03:09:34 +0000

    resolved session error

--- pwa/pages/_app.tsx
+++ pwa/pages/_app.tsx
@@ -4,7 +4,7 @@ import type { AppProps } from "next/app"
 import type { DehydratedState } from "react-query"
 import { SessionProvider } from "next-auth/react"
 
-function Root({ Component, pageProps: { session, ...pageProps } }: AppProps<{ dehydratedState: DehydratedState }>) {
+function Root({ Component, pageProps: { session, ...pageProps } }: AppProps<{ session: any; dehydratedState: DehydratedState }>) {
   return <Layout dehydratedState={pageProps.dehydratedState}>
     <SessionProvider session={session}>
       <Component {...pageProps} />
