0c069c2aea2775be28ce6beaf834048e6b7a6fd2
ethan-logue <ethanlogue03@gmail.com>
2025-01-22 03:11:09 +0000

    updated var type

--- pwa/pages/_app.tsx
+++ pwa/pages/_app.tsx
@@ -3,8 +3,9 @@ import Layout from "../components/common/Layout"
 import type { AppProps } from "next/app"
 import type { DehydratedState } from "react-query"
 import { SessionProvider } from "next-auth/react"
+import { Session } from "next-auth"
 
-function Root({ Component, pageProps: { session, ...pageProps } }: AppProps<{ session: any; dehydratedState: DehydratedState }>) {
+function Root({ Component, pageProps: { session, ...pageProps } }: AppProps<{ session: Session; dehydratedState: DehydratedState }>) {
   return <Layout dehydratedState={pageProps.dehydratedState}>
     <SessionProvider session={session}>
       <Component {...pageProps} />
