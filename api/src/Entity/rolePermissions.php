aa0b492e59b69a56aed112b67f60c3cab05d05a6
Casey Malley <casey@xeanto.us>
2025-01-26 20:31:48 +0000

    lol

---
+++ api/src/Entity/rolePermissions.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
+
+#[ORM\Entity]
+#[ApiResource]
+class rolePermissions
+{
+    #[ORM\Id]
+    #[ORM\ManyToOne(targetEntity: Role::class, inversedBy: 'rolePermissions')]
+    #[ORM\JoinColumn(name: 'roleID', referencedColumnName: 'roleID', nullable: false)]
+    public Role $role;
+
+    #[ORM\Id]
+    #[ORM\ManyToOne(targetEntity: Permission::class, inversedBy: 'rolePermissions')]
+    #[ORM\JoinColumn(name: 'permissionID', referencedColumnName: 'permissionID', nullable: false)]
+    public Permission $permission;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    public \DateTimeInterface $lastModified;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    public \DateTimeInterface $createdDate;
+
+    /**the permissions for this tole permissions*/
+    /**#[ORM\ManyToOne(inversedBy:'permissions')]
+    #[Asser\NotNull]
+    public ?rolePermissions $rolePermissions = null;*/
+
+    public function __construct()
+    {
+        $this->lastModified = new \DateTime();
+        $this->createdDate = new \DateTime();
+    }
+}
