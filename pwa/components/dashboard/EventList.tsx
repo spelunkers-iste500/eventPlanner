ae1f27933916d65bef6ab0dbc1ab7745ca82fac1
ethan-logue <ethanlogue03@gmail.com>
2025-02-08 23:48:42 +0000

    created ContextProvider to set content anywhere, some improvements to search button

--- pwa/components/dashboard/EventList.tsx
+++ pwa/components/dashboard/EventList.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import styles from "../dashboard/Dashboard.module.css";
 import { Button } from "@heroui/react";
 import { ArrowDownWideNarrow, ArrowUpWideNarrow, Search, XCircle } from "lucide-react";
@@ -15,31 +15,37 @@ const EventList: React.FC<EventListProps> = ({ heading, events }) => {
 	const [searchTerm, setSearchTerm] = useState("");
 	const [isExpanded, setIsExpanded] = useState(false);
 	const [reverseSorting, setReverseSorting] = useState(false);
+	const inputRef = useRef<HTMLInputElement>(null);
 
 	return (
 		<div className={styles.eventList}>
 			<div className={styles.eventListHeader}>
 				<h2>{heading}</h2>
 
-				{/* Search Bar Wrapper */}
-				<div className={styles.searchFilterContainer}>
-					<div
-						className={`${styles.searchWrapper} ${isExpanded ? styles.expanded : ""}`}
-						onFocus={() => setIsExpanded(true)}
-						onBlur={() => searchTerm === "" && setIsExpanded(false)}
-					>
+				<div className={styles.eventListButtons}>
+					{/* Search Bar Wrapper */}
+					<div className={`${styles.searchWrapper} ${isExpanded ? styles.expanded : ""}`}>
 						<input
+							ref={inputRef}
+							className={`${styles.searchInput} ${isExpanded ? styles.visible : ""}`}
+							onFocus={() => setIsExpanded(true)}
+							onBlur={() => searchTerm === "" && setIsExpanded(false)}
+							onChange={(e) => setSearchTerm(e.target.value)}
 							type="text"
 							placeholder="Search"
 							value={searchTerm}
-							onChange={(e) => setSearchTerm(e.target.value)}
-							className={`${styles.searchInput} ${isExpanded ? styles.visible : ""}`}
 						/>
-
+	
 							<button
 								aria-label="Search"
 								className={styles.iconWrapper}
-								onClick={isExpanded ? () => { setIsExpanded(false); setSearchTerm("") } : () => setIsExpanded(true)}
+								onClick={isExpanded ? () => {
+									setIsExpanded(false);
+									setSearchTerm("") 
+								} : () => {
+									setIsExpanded(true)
+									inputRef.current?.focus();
+								}}
 							>
 								{!isExpanded ? (
 									<Search size={20} />
@@ -48,10 +54,10 @@ const EventList: React.FC<EventListProps> = ({ heading, events }) => {
 								)}
 							</button>
 					</div>
-
+	
 					{/* Filter Button */}
 					<button
-						className={styles.filterIcon}
+						className={styles.iconWrapper}
 						aria-label="Sort"
 						onClick={() => setReverseSorting(!reverseSorting)}
 					>
