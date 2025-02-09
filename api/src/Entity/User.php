c520bfa57be390911a23bfbeedde6183ed17a74b
erb1851 <erb1851@rit.edu>
2025-02-09 19:01:31 +0000

    remove user test for now

--- api/src/Entity/User.php
+++ api/src/Entity/User.php
@@ -63,6 +63,8 @@ class User implements PasswordAuthenticatedUserInterface, UserInterface
         $this->roles = new ArrayCollection();
         $this->flights = new ArrayCollection();
         $this->organizations = new ArrayCollection();
+        $this->lastModified = new \DateTime();
+        $this->createdDate = new \DateTime();
     }
 
     public function getRoles(): array
