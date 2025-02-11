98200ba70a4a0f3bc32e6070dc69e0fb4d2fc81f
Casey Malley <casey@xeanto.us>
2025-02-11 16:24:23 +0000

    try to configure webpack to allow splatting stuff

--- pwa/next.config.js
+++ pwa/next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  crossOrigin: 'anonymous',
   output: 'standalone',
     webpack: (config) => {
       // camelCase style names from css modules
