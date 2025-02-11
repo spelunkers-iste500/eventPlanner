c548a6d0059c56a351f95a49c4459e6298af2ae8
Casey Malley <casey@xeanto.us>
2025-02-11 16:37:18 +0000

    send it to the moon idk man

--- pwa/webpack.config.js
+++ pwa/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     resolve: {
         alias: {
-            Utils: path.resolve(__dirname, 'src/utils/'),
+            'Utils/*': path.resolve(__dirname, 'utils/*'),
         }
     }
 }
