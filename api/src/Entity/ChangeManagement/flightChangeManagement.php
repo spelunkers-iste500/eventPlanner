20d70ccf1ba9e9fcb1f3382af99a41f47e72baa8
Casey Malley <casey@xeanto.us>
2025-02-26 05:39:43 +0000

    moving change management stuff to another namespace for now

---
+++ api/src/Entity/ChangeManagement/flightChangeManagement.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Entity\ChangeManagement;
+
+use App\Entity\Flight;
+
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
+
+#[ORM\Entity]
+// #[ApiResource]
+class flightChangeManagement
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(name: 'id', type: 'integer')]
+    public int $id;
+
+    //Relationships
+
+    //eventOrganization -> Event
+    #[ORM\ManyToOne(targetEntity: Flight::class)]
+    #[ORM\JoinColumn(name: 'flightID', referencedColumnName: 'id', nullable: true)]
+    public Flight $flight;
+
+    #[ORM\Column(length: 55)]
+    public string $versionNum;
+
+    #[ORM\Column(type: 'boolean')]
+    public bool $active;
+
+    #[ORM\Column(length: 255)]
+    public string $description;
+
+    #[ORM\Column(type: 'json')]
+    public array $beforeChanges;
+
+    #[ORM\Column(type: 'json')]
+    public array $afterChanges;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    public \DateTimeInterface $lastModified;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    public \DateTimeInterface $createdDate;
+
+    public function __construct()
+    {
+        $this->lastModified = new \DateTime();
+        $this->createdDate = new \DateTime();
+    }
+}
