71049d6b0c70207167a5a5b2e331d22e6bbc8a1f
ethan-logue <ethanlogue03@gmail.com>
2025-03-18 22:52:31 +0000

    registration and login completely working now, added user provider

--- pwa/utils/auth.ts
+++ pwa/utils/auth.ts
@@ -69,7 +69,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
                 otp: { label: "OTP", type: "text" },
             },
             async authorize(credentials, req) {
-                const apiUrl = (process.env.NEXTAUTH_URL?.endsWith("/")) ? process.env.NEXTAUTH_URL + "auth" : process.env.NEXTAUTH_URL + "/auth";
+                const apiUrl = (process.env.NEXTAUTH_URL?.includes('localhost')) ? "http://php/auth" : (process.env.NEXTAUTH_URL?.endsWith("/")) ? process.env.NEXTAUTH_URL + "auth" : process.env.NEXTAUTH_URL + "/auth";
 
                 const dbQuery = await pool.query("SELECT users.otp_secret, users.id FROM users WHERE email = $1", [credentials.email]);
                 if (dbQuery.rowCount === 0) {
@@ -116,8 +116,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
             async authorize(credentials, req) {
                 // var sslRootCAs = require('ssl-root-cas/latest')
                 // sslRootCAs.inject()
-                const apiUrl = (process.env.NEXTAUTH_URL?.endsWith("/")) ? process.env.NEXTAUTH_URL + "auth" : process.env.NEXTAUTH_URL + "/auth";
-
+                const apiUrl = (process.env.NEXTAUTH_URL?.includes('localhost')) ? "http://php/auth" : (process.env.NEXTAUTH_URL?.endsWith("/")) ? process.env.NEXTAUTH_URL + "auth" : process.env.NEXTAUTH_URL + "/auth";
+                // const apiUrl = 'http://php/auth'
                 // Add logic here to look up the user from the credentials supplied
                 // query the backend to get a jwt token
                 // if no token, return null
@@ -130,14 +130,12 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
                     email: credentials.email,
                     password: credentials.password,
                     headers: {
-                        "Content-Type": "application/json",
-                        "accept": "application/json",
+                        "Content-Type": "application/ld+json",
+                        "accept": "application/ld+json",
                     }
                 })
                 // bad password
                 if (authResponse.status >= 300 && authResponse.status < 500) {
-                    return null;
-                } else {
                     return null;
                 }
 
@@ -172,11 +170,11 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
             }
             return token;
     }
-    }
+    },
     // adapter: PostgresAdapter(pool),
-    // pages: {
-    //     signIn: "/login",
-    //     verifyRequest: "/verify",
-    // },
+    pages: {
+        signIn: "/login",
+        verifyRequest: "/verify",
+    },
     }
 )
