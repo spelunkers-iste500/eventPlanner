eb307e13ef24258900a8fc1bee395665ca911254
erb1851 <erb1851@rit.edu>
2025-03-23 19:17:27 +0000

    fixing data fixtures

---
+++ api/src/Factory/UserEventFactory.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\UserEvent;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+
+/**
+ * @extends PersistentProxyObjectFactory<UserEvent>
+ */
+final class UserEventFactory extends PersistentProxyObjectFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+    }
+
+    public static function class(): string
+    {
+        return UserEvent::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable
+    {
+        return [
+            'event' => null,
+            'isAccepted' => self::faker()->boolean(),
+            'user' => null,
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): static
+    {
+        return $this
+            // ->afterInstantiate(function(UserEvent $userEvent): void {})
+        ;
+    }
+}
