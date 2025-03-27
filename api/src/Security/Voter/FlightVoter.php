1ab8a701419d82bd160a6686593eb3312ae20b00
Casey Malley <casey@xeanto.us>
2025-03-27 03:38:31 +0000

    flight voter

---
+++ api/src/Security/Voter/FlightVoter.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Security\Voter;
+
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+final class FlightVoter extends Voter
+{
+    public const VIEW = 'view';
+
+    protected function supports(string $attribute, mixed $subject): bool
+    {
+        // replace with your own logic
+        // https://symfony.com/doc/current/security/voters.html
+        return in_array($attribute, [self::VIEW])
+            && $subject instanceof \App\Entity\Flight;
+    }
+
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+        // if the user is anonymous, do not grant access
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        // ... (check conditions and return true to grant permission) ...
+        switch ($attribute) {
+            case self::VIEW:
+                if ($subject->getUser() === $user) {
+                    return true;
+                }
+        }
+
+        return false;
+    }
+}
