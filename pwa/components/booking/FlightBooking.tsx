30d6eb6a3065180d05ccdde0ef83d7137526fa6c
Casey Malley <casey@xeanto.us>
2025-04-01 21:47:20 +0000

    adding getters to files

--- pwa/components/booking/FlightBooking.tsx
+++ pwa/components/booking/FlightBooking.tsx
@@ -13,7 +13,7 @@
 
 // The `handleSubmit` function is defined to handle the form submission event. It prevents the default form submission behavior and calls the `bookOffer` function.
 
-// The `bookOffer` function is an asynchronous function that sends a POST request to the `/flight_orders` endpoint with the selected flight offer data. 
+// The `bookOffer` function is an asynchronous function that sends a POST request to the `/flight_orders` endpoint with the selected flight offer data.
 // If the request is successful, it sets the content to the `Dashboard` component and displays a success notification using the `toaster` component.
 // If the request fails, it logs the error to the console and temporarily sets the content to the `Dashboard` component and displays a success notification.
 
@@ -27,18 +27,18 @@
 
 // Finally, the `FlightBooking` component is exported as the default export of the module.
 
-import React, { useEffect, useState } from 'react';
-import { toaster } from 'Components/ui/toaster';
-import { useSession } from 'next-auth/react';
-import { useBooking } from 'Utils/BookingProvider';
-import axios from 'axios';
-import styles from './EventForm.module.css';
-import { ArrowLeft, Calendar, User } from 'lucide-react';
-import FlightResults from './FlightResults';
-import { useContent } from 'Utils/ContentProvider';
-import Dashboard from 'Components/dashboard/Dashboard';
-import { useUser } from 'Utils/UserProvider';
-import { formatDateDisplay, formatTime } from 'Types/events';
+import React, { useEffect, useState } from "react";
+import { toaster } from "Components/ui/toaster";
+import { useSession } from "next-auth/react";
+import { useBooking } from "Utils/BookingProvider";
+import axios from "axios";
+import styles from "./EventForm.module.css";
+import { ArrowLeft, Calendar, User } from "lucide-react";
+import FlightResults from "./FlightResults";
+import { useContent } from "Utils/ContentProvider";
+import Dashboard from "Components/dashboard/Dashboard";
+import { useUser } from "Utils/UserProvider";
+import { formatDateDisplay, formatTime } from "Types/events";
 
 const FlightBooking = () => {
     const { bookingData, setBookingData } = useBooking();
@@ -47,8 +47,8 @@ const FlightBooking = () => {
     const { user } = useUser();
 
     const onPrevious = () => {
-		setBookingData({ ...bookingData, content: <FlightResults /> });
-	}
+        setBookingData({ ...bookingData, content: <FlightResults /> });
+    };
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
@@ -56,7 +56,7 @@ const FlightBooking = () => {
     };
 
     const bookingSuccess = () => {
-        setContent(<Dashboard />, 'Dashboard');
+        setContent(<Dashboard />, "Dashboard");
         toaster.create({
             title: "Flight Reserved",
             description: "Your flight has successfully been reserved.",
@@ -64,62 +64,77 @@ const FlightBooking = () => {
             duration: 3000,
         });
         updateEvent();
-    }
+    };
 
     const updateEvent = async () => {
-        axios.patch(`/user_events/${bookingData.userEventId}`, {
-            status: "accepted",
-        }, {
-            headers: {
-                'Authorization': `Bearer ${session?.apiToken}`,
-                'Content-Type': 'application/merge-patch+json',
-                'accept': 'application/ld+json',
-            }
-        })
-        .then((response) => {
-            console.log('Event updated:', response.data);
-        })
-        .catch((error) => {
-            console.error('Error updating event:', error);
-        });
-    }
-    
+        axios
+            .patch(
+                `/user_events/${bookingData.userEventId}`,
+                {
+                    status: "accepted",
+                },
+                {
+                    headers: {
+                        Authorization: `Bearer ${session?.apiToken}`,
+                        "Content-Type": "application/merge-patch+json",
+                        accept: "application/ld+json",
+                    },
+                }
+            )
+            .then((response) => {
+                console.log("Event updated:", response.data);
+            })
+            .catch((error) => {
+                console.error("Error updating event:", error);
+            });
+    };
+
     const bookOffer = async () => {
         if (!bookingData.selectedOffer) return;
 
-        axios.post(`/flight_orders`, {
-            offerId: bookingData.selectedOffer.id,
-            event: `/events/${bookingData.event.id}`,
-        }, {
-            headers: {
-                'Authorization': `Bearer ${session?.apiToken}`,
-                'Content-Type': 'application/ld+json',
-                'accept': 'application/ld+json',
-            }
-        })
-        .then((response) => {
-            console.log('Booking response:', response.data);
-            if (response.status == 201) {
-                bookingSuccess();
-            }
-        })
-        .catch((error) => {
-            console.error('Error fetching flight offers:', error);
+        axios
+            .post(
+                `/flight_orders`,
+                {
+                    offerId: bookingData.selectedOffer.id,
+                    event: `/events/${bookingData.event.id}`,
+                },
+                {
+                    headers: {
+                        Authorization: `Bearer ${session?.apiToken}`,
+                        "Content-Type": "application/ld+json",
+                        accept: "application/ld+json",
+                    },
+                }
+            )
+            .then((response) => {
+                console.log("Booking response:", response.data);
+                if (response.status == 201) {
+                    bookingSuccess();
+                }
+            })
+            .catch((error) => {
+                console.error("Error fetching flight offers:", error);
 
-            // temporarily here until api returns a 200
-            // bookingSuccess();
-        });
+                // temporarily here until api returns a 200
+                // bookingSuccess();
+            });
     };
 
     const capitalize = (string: string | undefined): string => {
-        if (!string) return '';
+        if (!string) return "";
         return string.charAt(0).toUpperCase() + string.slice(1);
     };
 
     return (
         <form className={styles.bookingForm} onSubmit={handleSubmit}>
             <div className={styles.resultsHeader}>
-                <button className={`text-btn ${styles.backBtn}`} onClick={onPrevious}><ArrowLeft /> Back</button>
+                <button
+                    className={`text-btn ${styles.backBtn}`}
+                    onClick={onPrevious}
+                >
+                    <ArrowLeft /> Back
+                </button>
                 <div>
                     <h2>Confirm Booking</h2>
                     <p>Verify all the listed information is correct.</p>
@@ -129,24 +144,64 @@ const FlightBooking = () => {
             <div className={styles.bookingInfo}>
                 <div>
                     <h3>Passenger Information</h3>
-                    <p><strong>Name:</strong> {user?.name}</p>
-                    <p><strong>Email:</strong> {user?.email}</p>
-                    <p><strong>Phone:</strong> {user?.phoneNumber}</p>
-                    <p><strong>Birthday:</strong> {formatDateDisplay(user?.birthday)}</p>
-                    <p><strong>Gender:</strong> {user?.gender === 'm' ? 'Male' : 'Female'}</p>
-                    <p><strong>Title:</strong> {capitalize(user?.title)}</p>
+                    <p>
+                        <strong>Name:</strong> {user?.name}
+                    </p>
+                    <p>
+                        <strong>Email:</strong> {user?.email}
+                    </p>
+                    <p>
+                        <strong>Phone:</strong> {user?.phoneNumber}
+                    </p>
+                    <p>
+                        <strong>Birthday:</strong>{" "}
+                        {formatDateDisplay(user?.birthday)}
+                    </p>
+                    <p>
+                        <strong>Gender:</strong>{" "}
+                        {user?.gender === "m" ? "Male" : "Female"}
+                    </p>
+                    <p>
+                        <strong>Title:</strong> {capitalize(user?.title)}
+                    </p>
                 </div>
                 <div>
                     <h3>Flight Information</h3>
-                    <p><strong>Trip:</strong> {bookingData.trip === 'round-trip' ? 'Round Trip' : 'One Way'}</p>
-                    <p><strong>Origin:</strong> {bookingData.originAirport}</p>
-                    <p><strong>Destination:</strong> {bookingData.destinationAirport}</p>
-                    <p><strong>Departure Date:</strong> {formatDateDisplay(bookingData.departDate)} • {formatTime(bookingData.selectedOffer?.slices[0].segments[0].departing_at)}</p>
-                    {bookingData.returnDate && <p><strong>Return Date:</strong> {formatDateDisplay(bookingData.returnDate)} • {formatTime(bookingData.selectedOffer?.slices[1].segments[0].departing_at)}</p>}
+                    <p>
+                        <strong>Trip:</strong>{" "}
+                        {bookingData.trip === "round-trip"
+                            ? "Round Trip"
+                            : "One Way"}
+                    </p>
+                    <p>
+                        <strong>Origin:</strong> {bookingData.originAirport}
+                    </p>
+                    <p>
+                        <strong>Destination:</strong>{" "}
+                        {bookingData.destinationAirport}
+                    </p>
+                    <p>
+                        <strong>Departure Date:</strong>{" "}
+                        {formatDateDisplay(bookingData.departDate)} •{" "}
+                        {formatTime(
+                            bookingData.selectedOffer?.slices[0].segments[0]
+                                .departing_at
+                        )}
+                    </p>
+                    {bookingData.returnDate && (
+                        <p>
+                            <strong>Return Date:</strong>{" "}
+                            {formatDateDisplay(bookingData.returnDate)} •{" "}
+                            {formatTime(
+                                bookingData.selectedOffer?.slices[1].segments[0]
+                                    .departing_at
+                            )}
+                        </p>
+                    )}
                 </div>
             </div>
 
-            <button type='submit'>Book Flight</button>
+            <button type="submit">Book Flight</button>
         </form>
     );
 };
