a96b09bfc346bb0642451a01772bf575da59fdc9
Steffen Barr <smb3402@rit.edu>
2025-04-10 18:36:36 +0000

    Created about us content

--- pwa/components/about/About.tsx
+++ pwa/components/about/About.tsx
@@ -1,16 +1,40 @@
-import React from 'react';
-import Container from '../common/Container';
-// import styles from './FAQ.module.css';
+import React from "react";
+import Container from "../common/Container";
+import styles from "./About.module.css";
 
-type FAQProps = {
+type TeamMember = {
+    name: string;
+    info: string;
 };
 
-const FAQ: React.FC<FAQProps> = ({  }) => {
+const teamMembers: TeamMember[] = [
+    { name: "Casey Malley", info: "Project Manager" },
+    { name: "Gavin Hunsinger", info: "Vice Project Manager" },
+    { name: "Ethan Logue", info: "Front End Developer" },
+    { name: "Steffen Barr", info: "Front End Developer" },
+    { name: "Eddie Brotz", info: "Quality Assurance" },
+    { name: "George Gabro", info: "Database Administrator" },
+    { name: "Noelle Voelkel", info: "Developer" },
+];
+
+const FAQ: React.FC = () => {
     return (
         <Container>
-            <h1>About Us</h1>
-            <h2>FAQ</h2>
-            <h2>Contact Us</h2>
+            <div className={styles.aboutPage}>
+                <h1 className={styles.title}>About Us</h1>
+                <h3 className={styles.teamTitle}>Team Spelunkers</h3>
+                <h2 className={styles.blurb}>
+                    Developers of this Event-Based Travel Planning System.
+                </h2>
+                <div className={styles.teamSection}>
+                    {teamMembers.map((member, index) => (
+                        <div key={index} className={styles.teamMember}>
+                            <span className={styles.name}>{member.name}</span>
+                            <span className={styles.info}>{member.info}</span>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </Container>
     );
 };
